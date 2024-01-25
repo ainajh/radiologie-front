c017b99d99bbe8aa11116565d8caaa6b80a0ab6a
menendezy22 <ranirisoajames@gmail.com>
2024-01-25 13:24:39 +0100

    old

---
+++ src/pages/schedule/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>hello</p>
+</template>
+
+<script>
+// Your script code goes here
+import { SchedulerPro } from "@bryntum/schedulerpro";
+</script>
+
+<style>
+/* Your style code goes here */
+</style>
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -12,7 +12,7 @@ export const useAuthStore = defineStore("authStore", {
   actions: {
     async login(role: String, data: any) {
       this.loading = true;
-      const { $api } = useNuxtApp();
+      const { $api } = useNuxtAppd();
       try {
         const response = await $api?.post(`user/login/${role}`, data);
         const result = response?.data;
